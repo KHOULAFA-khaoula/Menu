8233ab07d1975d13a8719593bb4b44b066fa43d1
ahmed-abouhijazi <61936147+ahmed-abouhijazi@users.noreply.github.com>
2021-12-01 23:48:30 +0100

    fix db using shared prefs

--- app/src/main/java/com/example/menu/DBSqlite.java
+++ app/src/main/java/com/example/menu/DBSqlite.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class DBSqlite extends SQLiteOpenHelper {
+    private static DBSqlite sInstance;
     private Context context;
     public static final String DB_NAME ="My_Menu.db";
     public static final  String Table_NameF = "restaurent";
@@ -16,7 +18,7 @@ public class DBSqlite extends SQLiteOpenHelper {
     public static final  String Table_NameD2 = "option";
     public static final  int DB_version = 1;
     public static final  String Column_id = "_id";
-    public static final  String Column_Nom = "nom";
+    public static final  String Column_Nom = "nomRest";
     public static final  String Column_Description = "descreption";
     public static final  String Column_Adresse = "adresse";
     public static final  String Column_Telephone = "telephone";
@@ -28,37 +30,36 @@ public class DBSqlite extends SQLiteOpenHelper {
     public static final  String Column_Speciality = "speciality";
     public static final  String Column_id_op = "_id";
     public static final  String Column_id_cat = "_id";
-    public static final  String Column_Nom_cat = "nom";
-    public static final  String Column_Nom_op = "nom";
+    public static final  String Column_Nom_cat = "nomCat";
+    public static final  String Column_Nom_op = "nomOpt";
 
-    public DBSqlite(@Nullable Context context) {
+    public static synchronized DBSqlite getInstance(Context context) {
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+        if (sInstance == null) {
+            sInstance = new DBSqlite(context.getApplicationContext());
+            sInstance.addData();
+        }
+        return sInstance;
+    }
+
+    private DBSqlite(@Nullable Context context) {
         super(context, DB_NAME, null, DB_version);
+        Log.d("const","in middle of construction");
         this.context = context;
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String queryF = "CREATE TABLE " +Table_NameF
-                +"("
-                +Column_id+" Integer primary key AUTOINCREMENT,"
-                +Column_Nom+" Text,"
-                +Column_Nom_cat+" Text,"
-                +Column_Description+" Text,"
-                +Column_Adresse+" Text,"
-                +Column_Telephone+" Text,"
-                +Column_Option+" INTEGER,"
-                +Column_Image+" Text,"
-                +Column_Menu_img+" Text,"
-                +Column_Reduction+" Text,"
-                +Column_Category+" INTEGER,"
-                +Column_Speciality+" Text,"
-                +"FOREIGN KEY("+Column_Option+") REFERENCES "+Table_NameD2+"("+Column_id_op+"),"
-                +"FOREIGN KEY("+Column_Nom_cat+") REFERENCES "+Table_NameD1+"("+Column_id_cat+"));";
+        Log.d("creat","in middle of construction");
         String queryD1 = "create table "+Table_NameD1+"("+Column_id_cat+" Integer primary key AUTOINCREMENT,"+Column_Nom_cat+" Text"+");";
         String queryD2 = "create table "+Table_NameD2+"("+Column_id_op+" Integer primary key AUTOINCREMENT,"+Column_Nom_op+" Text"+");";
+        String queryF  = "create table "+Table_NameF+"("+Column_id+" Integer primary key AUTOINCREMENT,"+Column_Nom+" Text,"+Column_Nom_cat+" Text,"+Column_Description+" Text,"+Column_Adresse+" Text,"+Column_Telephone+" Text,"+Column_Option+" Text,"+Column_Image+" Text,"+Column_Menu_img+" Text,"+Column_Reduction+" Text,"+Column_Category+" Text,"+Column_Speciality+" Text,"+"FOREIGN KEY("+Column_Option+") REFERENCES "+Table_NameD2+"("+Column_id_op+"),"+"FOREIGN KEY("+Column_Nom_cat+") REFERENCES "+Table_NameD1+"("+Column_id_cat+"));";
         db.execSQL(queryD1);
         db.execSQL(queryD2);
         db.execSQL(queryF);
+
     }
 
     @Override
@@ -78,16 +79,34 @@ public class DBSqlite extends SQLiteOpenHelper {
         db.insert(Table_NameD1,null,cv);
         cv.put(Column_Nom_cat,"COFFE");
         db.insert(Table_NameD1,null,cv);
-        cv.put(Column_Nom_op,"Sur place");
-        db.insert(Table_NameD2,null,cv);
-        cv.put(Column_Nom_op,"A emporter");
-        db.insert(Table_NameD2,null,cv);
-        cv.put(Column_Nom_op,"Traiteur");
-        long result = db.insert(Table_NameD2,null,cv);
+        ContentValues cv1 = new ContentValues();
+        cv1.put(Column_Nom_op,"Sur place");
+        db.insert(Table_NameD2,null,cv1);
+        cv1.put(Column_Nom_op,"A emporter");
+        db.insert(Table_NameD2,null,cv1);
+        cv1.put(Column_Nom_op,"Traiteur");
+        db.insert(Table_NameD2,null,cv1);
+        ContentValues cv2 = new ContentValues();
+        cv2.put(Column_Nom,"Le Gouthé");
+        cv2.put(Column_Nom_cat,"COFFE");
+        cv2.put(Column_Description,"Cuisine maison, plats raffinés et classiques, ambiance décontractée et conviviale, café familial.");
+        cv2.put(Column_Adresse,"Villa 92 , quartier mabroka (abwab gueliz)\n" +
+                "Marrakech");
+        cv2.put(Column_Telephone,"0661965115");
+        cv2.put(Column_Option,"Sur place");
+        cv2.put(Column_Image,"src/main/res/drawable-v24/gouth.jpg");
+        cv2.put(Column_Menu_img,"src/main/res/drawable-v24/gouth1.jpg");
+        cv2.put(Column_Reduction,"0%");
+        cv2.put(Column_Category,"COFFE");
+        Log.d("middle","in middle of creation");
+        cv2.put(Column_Speciality,"Petit-déjeuner, Café, Pizza, Tacos, Crêpes");
+        long result = db.insert(Table_NameF,null,cv2);
         if(result == -1){
             Toast.makeText(context,"failed",Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(context,"Added successfully!",Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -3,21 +3,26 @@ package com.example.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        SharedPreferences prefs = getSharedPreferences("prefs",MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart",true);
+        if (firstStart){
+            InsertData();
+        }
     }
 
     public void didTapButton(View view) {
@@ -29,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
         myAnim.setInterpolator(interpolator);
 
         button.startAnimation(myAnim);
-        DBSqlite inst = new DBSqlite(MainActivity.this);
-        inst.addData();
+
         startActivity(new Intent(MainActivity.this, Home.class));
 
 
+    }
+
+    private void InsertData(){
+        DBSqlite inst = DBSqlite.getInstance(MainActivity.this);
+        Log.d("creation","Accessed To DB");
+        SharedPreferences prefs = getSharedPreferences("prefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart",false);
+        editor.apply();
     }
 }
